fe0ff859298be22a019695fd0aa1ac20b6c9915c
Sasida D Wijenandana <260614285+sasidadilhancosmetics-lang@users.noreply.github.com>
2026-09-23 15:53:26 +0530

    bug fixed

--- lib/erpnext-sync.ts
+++ lib/erpnext-sync.ts
@@ -1284,7 +1284,7 @@ export async function syncOrderDeliveryPaymentEntriesToErp(
     return createDeliveryPaymentEntry(order, location, completedAt, {
       mopNameOverride: options.mopNameOverride,
       requireMop: options?.requireMop,
-      paidAmount: options.paidAmount,
+      paidAmount: options?.paidAmount,
     });
   }
 
@@ -1292,7 +1292,7 @@ export async function syncOrderDeliveryPaymentEntriesToErp(
     return createDeliveryPaymentEntry(order, location, completedAt, {
       requireMop: options?.requireMop,
       mopNameOverride: options?.mopNameOverride,
-      paidAmount: options.paidAmount,
+      paidAmount: options?.paidAmount,
     });
   }
 
